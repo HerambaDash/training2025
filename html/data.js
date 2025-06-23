4379ff820eba189a5f40916a121d02211a654195
HerambaDash <heramba.dash@squbix.com>
2025-06-23 10:40:40 +0530

    dom methods

---
+++ html/data.js
@@ -0,0 +1,42 @@
+const itemList = [
+  "Alpha", "Beta", "Gamma", "Delta", "Epsilon",
+  "Zeta", "Eta", "Theta", "Iota", "Kappa"
+];
+
+const cities = {
+  delhi: {
+    "Connaught Place": 110001,
+    "Chanakyapuri": 110021,
+    "Karol Bagh": 110005,
+    "Saket": 110017,
+    "Dwarka": 110075
+  },
+  mumbai: {
+    "Bandra": 400050,
+    "Andheri": 400053,
+    "Juhu": 400049,
+    "Powai": 400076,
+    "Colaba": 400005
+  },
+  chennai: {
+    "T. Nagar": 600017,
+    "Velachery": 600042,
+    "Adyar": 600020,
+    "Guindy": 600032,
+    "Anna Nagar": 600040
+  },
+  kolkata: {
+    "Salt Lake": 700091,
+    "Dumdum": 700028,
+    "Park Street": 700016,
+    "Behala": 700034,
+    "New Alipore": 700053
+  },
+  bangalore: {
+    "Indiranagar": 560038,
+    "Koramangala": 560034,
+    "Whitefield": 560066,
+    "MG Road": 560001,
+    "Jayanagar": 560041
+  }
+};
